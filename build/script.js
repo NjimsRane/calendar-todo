4459e8c9018fa1d455b4e30974e3bb5041f5d9d3
NjimsRane <njims2011@gmail.com>
2023-05-06 05:23:49 +0100

    adding comments on js

--- build/script.js
+++ build/script.js
@@ -8,28 +8,23 @@ let today = new Date();
 let month = today.getMonth();
 let year = today.getFullYear();
 
-const months = [
-    'January',
-    'february',
-    'march',
-    'april',
-    'may',
-    'june',
-    'july',
-    'august',
-    'september',
-    'october',
-    'november',
-    'december',
-];
+const months = ['January', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
 
 // funcion to add days
 
 const initCalendar = () => {
+    // first day of the current month
     const firstDay = new Date(year, month, 1);
+
+    // last day of the current month
     const lastDay = new Date(year, month + 1, 0);
+
+    // last day of the prev month
     const prevlastDay = new Date(year, month, 0);
+    console.log(prevlastDay);
+
     const prevDays = prevlastDay.getDate();
+    console.log(prevDays);
     const lastDate = lastDay.getDate();
     const day = firstDay.getDate();
     const nextDays = 7 - lastDay.getDay() - 1;
@@ -42,17 +37,13 @@ const initCalendar = () => {
         days += `<div class='day prev-date'>${prevDays - i + 1}</div>`;
     }
     for (let i = day; i < lastDate; i++) {
-
-        if (
+        (
             i === new Date().getDate() &&
             year === new Date().getFullYear() &&
             month === new Date().getMonth()
-        ) {
-            days += `<div class='day today'>${i}</div>`;
-        } else {
-            days += `<div class='day'>${i}</div>`;
-        }
-
+        )
+            ? days += `<div class='day today'>${i}</div>`
+            : days += `<div class='day'>${i}</div>`;
     }
 
 
